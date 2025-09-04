c54549a737af02acf80118231f51b34549a26d7e
chigoziem <chigoziemi@hotmail.com>
2025-09-04 04:36:03 +0100

    admin dashboard

---
+++ app/api/blog/route.js
@@ -0,0 +1,49 @@
+// Apis to manage blogs posts
+
+import { ConnectDB } from "@/lib/config/db";
+import BlogModel from "@/lib/models/BlogModel";
+const { NextResponse } = require("next/server");
+import {writeFile} from 'fs/promises'
+
+const LoadDB = async () => {
+    // Simulate database loading
+    await ConnectDB();
+    console.log("Database Loaded");
+};
+
+// Load the database when the module is imported
+// This ensures the DB connection is established once
+LoadDB();
+
+export async function GET(request) {
+    // console.log("Blog GET Hit");
+    return NextResponse.json({message: "API Working"});
+}
+
+export async function POST(request) {
+
+    const formData = await request.formData();
+    const timestamp = Date.now();
+
+    const image = formData.get('image');
+    const imageByteData = await image.arrayBuffer();
+    const buffer = Buffer.from(imageByteData);
+    const path = `./public/${timestamp}_${image.name}`;
+    await writeFile(path,buffer);
+    const imgUrl = `/${timestamp}_${image.name}`;
+    // console.log(imgUrl);
+
+        const blogData = {
+            title:`${formData.get('title')}`,
+            description:`${formData.get('description')}`,
+            category:`${formData.get('category')}`,
+            author:`${formData.get('author')}`,
+            image: `${imgUrl}`,
+            authorImg:`${formData.get('authorImg')}`
+        }
+
+        await BlogModel.create(blogData);
+        console.log("Blog Saved")
+
+    return NextResponse.json({success:true,msg:"Blog Added"})
+}
